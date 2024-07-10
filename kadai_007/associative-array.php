19d024ec5d3a69bf308a0f015b8588186f827f8c
yutak215 <31120122+yutak215@users.noreply.github.com>
2024-07-11 06:03:24 +0900

    Add files via upload
    kadai_007をプッシュ

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        $vege1 = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($vege1);
+        ?>
+    </p>
+</body>
+</html>
